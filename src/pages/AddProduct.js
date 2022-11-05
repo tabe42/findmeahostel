6f0d80c07cc9e786ae242d67935f1482e4ecda1e
Tobin <50885993+tabe42@users.noreply.github.com>
2022-11-06 00:42:39 +0530

    adding walk time api call

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -15,7 +15,7 @@ export const AddProduct = ({ isAuth }) => {
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
 
-
+  
 
   const [imageinput, setImageinput] = useState(null);
   const [imageurl, setImageurl] = useState("https://cdn.dribbble.com/users/989466/screenshots/16168689/media/66899610428d098a4467516591ce01ae.png?compress=1&resize=400x300");
@@ -29,6 +29,9 @@ export const AddProduct = ({ isAuth }) => {
   const [area, setArea] = useState(100);
   const [rent, setRent] = useState(0);
   const [phone, setPhone] = useState(0)
+  const [college, setCollege] = useState("")
+  const [walkingTime, setWalkingTime] = useState(0)
+  const [buildingType, setbuildingType] = useState("")
 
   const productCollectionRef = collection(db, "products");
   let navigate = useNavigate();
@@ -44,7 +47,7 @@ const handle = (e) => {
   setData(newdata);
   console.log(newdata);
 };
-
+let walk=0
 function submit(e) {
   e.preventDefault();
   console.log("submitting",lat,lng)
@@ -58,6 +61,9 @@ function submit(e) {
     phone:phone,
     rent:rent,
     url:imageurl,
+    buildingType:buildingType,
+    college:college,
+    walktime:walk
   })
     .then((res) => console.log(res.data))
     .catch((e) => console.log("Error encountered: ", e));
@@ -75,6 +81,20 @@ function submit(e) {
   //   });
   //   // navigate("/products");
   // };
+
+const getWalkTime = async () =>{
+  let url="https://api.mapbox.com/directions-matrix/v1/mapbox/walking/"+lng+","+lat+";"+"76.328516463327"+","+"10.028373499551039"+"?sources=1&annotations=distance,duration&access_token=pk.eyJ1IjoidG9iYWJlIiwiYSI6ImNsN3BybnhpZjBmYWY0MXM3bGc3Yzd1eGcifQ.p3whIN6-M7IqOJF47PtmZg"
+  console.log("url:",url)
+  await Axios.get(url)
+  .then(function (response) {
+    walk=response.data.durations[0][0]
+    console.log("got", walk)
+  })
+}
+
+
+
+
   let staticUrl = ""
   const uploadImage = async () => {
     if (imageinput == null)
@@ -89,8 +109,9 @@ function submit(e) {
         staticUrl = url
         // addProduct(url, imgname);
         console.log("reached here")
-      }).then(()=>{
+      }).then(()=>getWalkTime()).then(()=>{
         console.log("submitting",imageurl,lng)
+        console.log("submitting",walk)
         Axios.post("https://shrouded-plateau-82529.herokuapp.com/https://backend-findmeahostel.herokuapp.com/tasks/add/", {
           lat: lat,
           lon: lng,
@@ -101,6 +122,9 @@ function submit(e) {
           phone:phone,
           rent:rent,
           url:staticUrl,
+          buildingType:buildingType,
+          college:college,
+          walktime:walk
         })
           .then((res) => {
             console.log(res.data)
@@ -122,10 +146,11 @@ function submit(e) {
   });
 
   return (
-    <div className="h-auto flex flex-col-reverse md:flex-row items-center justify-center p-4">
+    <div className="h-auto flex flex-col-reverse md:flex-row items-center justify-center p-4 max-w-screen">
       {/* <FormComponent lat={lat} lng={lng} setLat={setLat} setLng={setLng} data={data} setData={setData} handle={handle} submit={submit}/> */}
       <div className="m-auto">
         <div className="flex flex-col space-y-4 justify-center   bg-gray-100  shadow-md rounded-md p-8 w-fit h-fit">
+          <label for="files">Upload image</label>
           <input
             required
             id="files"
@@ -162,6 +187,37 @@ function submit(e) {
           />
           <div>
 
+          <select
+            required
+            type="number"
+            placeholder="College"
+            className="border-2 p-2 rounded-md w-1/2"
+            onChange={(e) => {
+              setCollege(e.target.value);
+            }}
+            >
+              <option value="" disabled selected>Select College</option>
+              <option value="MEC">MEC</option>
+              <option value="TKM">TKM</option>
+              <option value="CET">CET</option>
+            </select>
+          <select
+            required
+            type="number"
+            className="border-2 p-2 rounded-md w-1/2"
+            onChange={(e) => {
+              setbuildingType(e.target.value);
+            }}
+            >
+              <option value="" disabled selected>Select building type</option>
+              <option value="Apartment">Apartment</option>
+              <option value="House">House</option>
+              <option value="Room">Room</option>
+            </select>
+   
+            </div>
+            <div>
+
           <input
             required
             type="number"
@@ -178,22 +234,31 @@ function submit(e) {
             className="border-2 p-2 rounded-md w-1/3"
             onChange={(e) => {
               setBath(e.target.value);
-            }}
-            />
-          <input
-            required
-            type="number"
-            placeholder="Area in sqft"
-            className="border-2 p-2 rounded-md w-1/3"
-            onChange={(e) => {
-              setArea(e.target.value);
-            }}
-            />
-            </div>
+          }}
+          />
+        <input
+          required
+          type="number"
+          placeholder="Area in sqft"
+          className="border-2 p-2 rounded-md w-1/3"
+          onChange={(e) => {
+            setArea(e.target.value);
+          }}
+          />
+          </div>
           <textarea
+            
+            rows="4"
+            className="border-2 p-2 rounded-md  resize-none md:hidden"
+            placeholder="Address of property"
+            onChange={(e) => {
+              setAddress(e.target.value);
+            }}
+          />
+          <textarea
+            rows="4"
             cols="40"
-            rows="9"
-            className="border-2 p-2 rounded-md resize-none"
+            className="border-2 p-2 rounded-md resize-none hidden md:flex"
             placeholder="Address of property"
             onChange={(e) => {
               setAddress(e.target.value);
@@ -207,7 +272,8 @@ function submit(e) {
           </button>
         </div>
       </div>
-      <DraggableMap lat={lat} lng={lng} setLat={setLat} setLng={setLng}/>
+      <DraggableMap lat={lat} lng={lng} setLat={setLat} setLng={setLng} />
+      <h3 className="font-bold text-md -my-2 md:hidden">Drag marker to property location</h3>
     </div>
   );
 };
